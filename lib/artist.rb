d23c89f445ae95e34d0da59556446cb2428bb8d7
thomas <tom.angotti11@gmail.com>
2023-02-01 14:30:50 -0700

    commit tree b9d9ebc33ac29429a6ba7f8fcc850e51128f44b2

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,18 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Artist
+
+  
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
@@ -11,32 +21,23 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
   end
 
-  def self.count
-    self.all.count
-  end
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 
 class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
 
@@ -10,27 +19,14 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
 
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+
 end
